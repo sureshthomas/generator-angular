e38e0ae92a6b4ba97a6edb33e72309936201e52b
suresh.thomas@yahoo.com <Leena@123>
2019-03-14 11:07:04 +0000

    sas :wq

---
+++ udaciMeals/app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name udacMealsApp
+ * @description
+ * # udacMealsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('udacMealsApp', []);
---
+++ udaciMeals/app/scripts/controllers/menu.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name udacMealsApp.controller:MenuCtrl
+ * @description
+ * # MenuCtrl
+ * Controller of the udacMealsApp
+ */
+angular.module('udacMealsApp')
+  .controller('MenuCtrl', function () {
+
+    this.id='strawberry-pudding';
+    this.name='Strawberry Pudding';
+    this.img='strawberry-pudding.img';
+    this.rating='5';
+
+  });
